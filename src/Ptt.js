8fe9184f234fed34fdac86a832ce977b17ab67d3
黃志仁 <tbsthitw@gmail.com>
2019-02-04 03:26:32 +0800

    Sortable favorite list !

--- src/Ptt.js
+++ src/Ptt.js
@@ -9,6 +9,20 @@ import {
 } from 'events';
 
 export default class Ptt extends EventEmitter {
+	static SocketEvents = [
+		'connect',
+		'disconnect',
+		'message',
+		'error',
+	];
+
+	static typeMap = {
+		"◎": "board",
+		"Σ": "group",
+		"□": "folder",
+		"--": "divider"
+	};
+
 	constructor() {
 		super();
 		this.state = {
@@ -22,12 +36,12 @@ export default class Ptt extends EventEmitter {
 		});
 		this.term.state.setMode('stringWidth', 'dbcs');
 		this.term.getLine = i => this.term.state.getLine(i).str;
-		this.socket
-			.on("connect", () => this.emit("connect"))
-			.on("message", (data) => {
-				this.term.write(data);
-				console.log(this.term.toString());
-			});
+		this.on("message", (data) => {
+			this.term.write(data);
+			console.log(this.term.toString());
+		});
+
+		Ptt.SocketEvents.forEach(e => this.socket.on(e, this.emit.bind(this, e)));
 	}
 
 	async connect() {
@@ -40,16 +54,17 @@ export default class Ptt extends EventEmitter {
 	}
 
 	async login(account) {
-		await this.socket.sendline(account.username + "\r" + account.password);
+		await this.sendline(account.username + "\r" + account.password);
 		let ret = null,
 			_;
 		return new Promise(resolve => {
-			this.socket.on("message", (_ = async (data) => {
+			this.on("message", (_ = async (data) => {
+				console.warn(data);
 				ret = await this.isLogin(data, account);
 				if (typeof ret === "boolean") {
 					this.state.login = ret;
 					this.emit("login", ret);
-					this.socket.removeListener("message", _);
+					this.removeListener("message", _);
 					resolve(ret);
 				}
 			}));
@@ -65,27 +80,21 @@ export default class Ptt extends EventEmitter {
 		} else if (term.includes("抱歉，目前已有太多 guest 在站上")) {
 			return false;
 		} else if (term.includes("您想刪除其他重複登入的連線嗎")) {
-			await this.socket.sendline(logout_dup ? "Y" : "n");
+			await this.sendline(logout_dup ? "Y" : "n");
 		} else if (term.includes("請勿頻繁登入以免造成系統過度負荷")) {
-			await this.socket.sendline();
+			await this.send(" ");
 		} else if (term.includes("按任意鍵繼續")) {
-			await this.socket.sendline();
+			await this.send(" ");
 		} else if (term.includes("您要刪除以上錯誤嘗試的記錄嗎")) {
-			await this.socket.sendline(delete_try_log ? "Y" : "n");
-		} else if (term.includes("我是")) {
+			await this.sendline(delete_try_log ? "Y" : "n");
+		} else if (term.includes("主功能表")) {
 			return true;
 		}
 		return null;
 	}
 
 	async getFavorite() {
-		const typeMap = {
-			"◎": "board",
-			"Σ": "group",
-			"□": "folder",
-			"--" : "divider"
-		};
-		await this.socket.send(KEYS.CtrlZ + "f" + KEYS.Home);
+		await this.send(KEYS.CtrlZ + "f" + KEYS.Home);
 		const favorites = [];
 		let firstId = -1,
 			endOfList = false;
@@ -99,7 +108,7 @@ export default class Ptt extends EventEmitter {
 					read: line.substrWidth(8, 2).trim() === '',
 					boardname: line.substrWidth(10, 12).trim(),
 					class: line.substrWidth(23, 4).trim(),
-					type: typeMap[line.substrWidth(28, 2).trim()],
+					type: Ptt.typeMap[line.substrWidth(28, 2).trim()],
 					sign: line.substrWidth(28, 2).trim(),
 					title: line.substrWidth(28, 31),
 					nuser: parseInt(line.substrWidth(62, 5).trim()) || line.substrWidth(62, 5).trim(),
@@ -115,8 +124,34 @@ export default class Ptt extends EventEmitter {
 				favorites.push(favorite);
 			}
 			if (endOfList) break;
-			await this.socket.send(KEYS.PgDown);
+			await this.send(KEYS.PgDown);
 		}
 		return favorites;
+	}
+
+	changeFavoriteOrder(originId, newId) {
+		this.send(KEYS.CtrlZ + "f");
+		this.sendline(originId);
+		this.send("M");
+		this.sendline(newId);
+	}
+
+	FavoriteAppendDivider(lineId) {
+		this.send(KEYS.CtrlZ + "f");
+		this.sendline(lineId);
+		this.send("L");
+	}
+
+	async send(data) {
+		!data.endsWith("\r") && console.log("SEND:", data);
+		return new Promise(resolve => {
+			this.socket.send(data);
+			this.once('message', resolve);
+		});
+	}
+
+	async sendline(data) {
+		console.log("SENDLINE:", data);
+		return this.send(data + "\r");
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "babel-polyfill";
 import Ptt from "./Ptt";
 import dom, { Fragment } from 'jsx-render';
+import * as toastr from 'toastr';
 
 const account = {
 	username: "guest",
@@ -9,64 +10,122 @@ const account = {
 	delete_try_log: true
 };
 
-(async function () {
-	const ptt = new Ptt();
-	const userStatus = (<a class="right small" href="#!">連線中...</a>);
-	topbar.appendChild(userStatus);
-	ptt.on("login", success => userStatus.innerText = success ? `我是 ${account.username}` : "登入失敗 QQ");
-	ptt.on("connect", () => userStatus.innerText = "連線成功，登入中...");
+const ptt = new Ptt();
+ptt.on("login", success => {
+	if (success) {
+		toastr.success("登入成功");
+		userStatus.innerText = `我是 ${account.username}`;
+	} else {
+		toastr.error("登入失敗 QQ");
+		userStatus.innerText = "登入失敗 QQ";
+	}
+});
+ptt.on("connect", () => userStatus.innerText = "連線成功，登入中...");
+ptt.on("disconnect", () => toastr.error("斷線了 88888"));
 
-	const favoriteBtn = (<a class="btn" href="#">最愛看版</a>);
-	favoriteBtn.onclick = async () => {
-		if (!ptt.state.login) {
-			alert("還在登入欸");
-			return;
-		}
-		const boardList = document.querySelector(".b-list-container");
-		boardList.innerHTML = "";
+const userStatus = (<a class="right small" href="#!">連線中...</a>);
+topbar.appendChild(userStatus);
 
-		ptt.state.login && ptt.getFavorite()
-			.then(list => {
-				list.forEach(board => {
-					switch (board.type) {
-						case "board":
-							boardList.appendChild(
-								<div class="b-ent">
-									<a class="board" data-href={`/bbs/${board.boardname}/index.html`} href="#!">
-										<div class="board-name">{board.boardname}</div>
-										<div class="board-nuser"><span class="hl">{board.nuser}</span></div>
-										<div class="board-class">{board.class}</div>
-										<div class="board-title">{board.title}</div>
-									</a>
-								</div>
-							)
-							break;
-						case "divider":
-							boardList.appendChild(<div class="b-ent"><hr /></div>);
-							break;
-						case "folder":
-						case "group":
-							boardList.appendChild(
-								<div class="b-ent">
-									<a class="board">
-										<div class="board-name">{board.boardname}</div>
-										<div class="board-nuser"><span class="hl">{board.nuser}</span></div>
-										<div class="board-class">{board.class}</div>
-										<div class="board-title">{board.title}</div>
-									</a>
-								</div>
-							)
-							break;
-						default:
-							break;
-					}
+const favoriteBtn = (<a class="btn" href="#">最愛看版</a>);
+favoriteBtn.onclick = async () => {
+	if (!ptt.state.login) {
+		toastr.warning("還在登入欸")
+		return;
+	}
+	const boardList = document.querySelector(".b-list-container");
+	boardList.innerHTML = '<div style="margin:auto;width:fit-content;"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>';
+
+	ptt.state.login && ptt.getFavorite()
+		.then(list => {
+			boardList.innerHTML = "";
+			list.forEach(board => {
+				switch (board.type) {
+					case "board":
+						boardList.appendChild(DragDroper(
+							<div class="b-ent" id={`favorite-${board.id}`} data-id={board.id} draggable>
+								<a class="board" data-href={`/bbs/${board.boardname}/index.html`}>
+									<div class="board-name">{board.boardname}</div>
+									<div class="board-nuser"><span class="hl">{board.nuser}</span></div>
+									<div class="board-class">{board.class}</div>
+									<div class="board-title">{board.title}</div>
+								</a>
+							</div>
+						));
+						break;
+					case "divider":
+					case "folder":
+					case "group":
+						boardList.appendChild(DragDroper(
+							<div class="b-ent" id={`favorite-${board.id}`} data-id={board.id} draggable>
+								<a class="board">
+									<div class="board-name">{board.boardname}</div>
+									<div class="board-nuser"><span class="hl">{board.nuser}</span></div>
+									<div class="board-class">{board.class}</div>
+									<div class="board-title">{board.title}</div>
+								</a>
+							</div>
+						));
+						break;
+					default:
+						break;
 				}
-				)
-
 			})
-	};
-	document.querySelector('.btn-group-cls').appendChild(favoriteBtn);
+		})
+};
+document.querySelector('.btn-group-cls').appendChild(favoriteBtn);
 
+// drag drop board
+var dragSrcEl = null;
+function handleDragStart(e) {
+	this.style.opacity = 0.4;
+	dragSrcEl = e.target;
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('text/plain', this.id);
+}
+function handleDragOver(e) {
+	e.preventDefault();
+	this.style.borderTop = "2px solid gray";
+	e.dataTransfer.dropEffect = 'move';
+	return false;
+}
+function handleDragLeave(e) {
+	this.style.borderTop = "";
+}
+function handleDrop(e) {
+	e.stopPropagation();
+	if (dragSrcEl != this) {
+		var sourceId = e.dataTransfer.getData('text/plain');
+		this.parentNode.insertBefore(document.getElementById(sourceId), this);
+		var dropElem = this.previousSibling;
+		if (dragSrcEl.dataset.id < this.dataset.id)
+			ptt.changeFavoriteOrder(dragSrcEl.dataset.id, this.dataset.id - 1);
+		else
+			ptt.changeFavoriteOrder(dragSrcEl.dataset.id, this.dataset.id);
+		console.log("order", dragSrcEl.dataset.id, this.dataset.id);
+		DragDroper(dropElem);
+
+		document.querySelectorAll(".b-ent").forEach((elem, i) => {
+			elem.dataset.id = i + 1;
+		});
+	}
+	this.style.borderTop = "";
+	return false;
+}
+function handleDragEnd(e) {
+	if (dragSrcEl) dragSrcEl.style.opacity = "";
+	this.style.borderTop = "";
+}
+function DragDroper(elem) {
+	elem.addEventListener('dragstart', handleDragStart, false);
+	elem.addEventListener('dragover', handleDragOver, false);
+	elem.addEventListener('dragleave', handleDragLeave, false);
+	elem.addEventListener('drop', handleDrop, false);
+	elem.addEventListener('dragend', handleDragEnd, false);
+	return elem;
+}
+
+(async function () {
+	// start ptt
 	await ptt.connect();
 	!ptt.state.login && await ptt.login(account);
 })();
